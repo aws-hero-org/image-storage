7ec9a6de99809aac0fac9a5d7dea89d44c1a3f45
Florian Siegel <florian.siegel@netlight.com>
2023-05-28 16:31:43 +0200

    Initial commit

---
+++ test/image-storage.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ImageStorage from '../lib/image-storage-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/image-storage-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ImageStorage.ImageStorageStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
